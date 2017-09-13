1d6dd73ac32d235d5be2b3576751ca08b1e5df0a
Raimo Geisel <raimog92@web.de>
2017-09-13 21:19:49 +0200

    Charset now UTF-8

--- js/navBar.js
+++ js/navBar.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+﻿/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleNavBar() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
